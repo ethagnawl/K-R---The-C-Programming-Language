1e32bbfd57e08e6c100dea7b0ebe2688be2dca47
Peter Doherty <pdoherty926@gmail.com>
2017-01-17 17:02:03 -0500

    chapter-1 - exercise 1.7 - print the value of EOF

--- chapter-1/copy-input-to-output.c
+++ chapter-1/copy-input-to-output.c
@@ -4,7 +4,7 @@ main() {
     int c;
 
     while((c = getchar()) != EOF) {
-        printf("%d\n", (c != EOF));
+        printf("%d\n", (c != EOF));     // prints 1
     }
-    printf("%d\n", (getchar() != EOF));
+    printf("%d\n", EOF); // ^d prints -1
 }
