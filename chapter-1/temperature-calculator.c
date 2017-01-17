534f9cf27ea65318e4d7b77e20cd09229325e992
Peter Doherty <pdoherty926@gmail.com>
2017-01-16 22:42:27 -0500

    chapter-1 - exercise 1-4

--- chapter-1/temperature-calculator.c
+++ chapter-1/temperature-calculator.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// print Fahrenheit-Celcius table
+// print Fahrenheit - Celcius table
 // for fahr = 0..300
 
 main () {
@@ -14,11 +14,20 @@ main () {
     step = 20;
     fahr = lower;
 
-    printf("Fahrenheit-Celcius\n");
+    printf("Fahrenheit - Celcius\n");
     printf("==========\n");
     while(fahr <= upper) {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
         printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
+    }
+
+    printf("Celcius - Fahrenheit \n");
+    printf("==========\n");
+    celsius = lower;
+    while(celsius <= upper) {
+        fahr = (9.0 / 5.0) * celsius + 32.0;
+        printf("%3.0f %6.1f\n", celsius, fahr);
+        celsius = celsius + step;
     }
 }
