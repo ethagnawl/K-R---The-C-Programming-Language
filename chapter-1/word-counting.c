bf6d40ab15d0cc7991cf7c98170b669c767491ce
Peter Doherty <pdoherty926@gmail.com>
2017-01-17 22:25:41 -0500

    chapter-1 - exercise 1-11 - testing strategy for wc clone

--- chapter-1/word-counting.c
+++ chapter-1/word-counting.c
@@ -29,3 +29,16 @@ main() {
     printf("%d %d %d\n", nl, nw, nc);
 }
 
+// 1-11
+// (It should go without saying that all of the following should be
+// automated.)
+// 1. empty string
+// 2. string starting with each of the special characters
+// 3. string ending with each of the special characters
+// 4. string starting and ending with each of the special characters
+// 5. string starting and ending with repeating sequences of each of the
+// special characters
+// 6. ridiculously long string
+// 7. contents of dict
+// 8. language keywords
+// 9. big list of naugty strings?
