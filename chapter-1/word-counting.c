112f0a554a9726f52377c07794fd65434737ae7d
Peter Doherty <pdoherty926@gmail.com>
2017-01-17 22:40:08 -0500

    chapter-1 - example 1-11 - update testing strategy for wc clone

--- chapter-1/word-counting.c
+++ chapter-1/word-counting.c
@@ -33,12 +33,20 @@ main() {
 // (It should go without saying that all of the following should be
 // automated.)
 // 1. empty string
-// 2. string starting with each of the special characters
-// 3. string ending with each of the special characters
-// 4. string starting and ending with each of the special characters
-// 5. string starting and ending with repeating sequences of each of the
-// special characters
-// 6. ridiculously long string
-// 7. contents of dict
-// 8. language keywords
-// 9. big list of naugty strings?
+// 2. input containing each special character
+// 3. input containing every special character
+// 4. string starting with each of the special characters
+// 5. string ending with each of the special characters
+// 6. string starting and ending with each of the special characters
+// 7. string starting and ending with repeating sequences of each of the
+// 8. randomly generated strings of random length composed of a fixed
+// zet of words and the three special characters.
+// zpecial characters
+// 11. ridiculously long string containing none of the special characters
+// 12. ridiculously long string containing all of the special characters
+// 13. three ridiculously long strings, each containing one of the special characters
+// 14. three ridiculously long strings, each containing all of the special characters
+// 15. contents of dict
+// 16. language keywords
+// 17. big list of naugty strings?
+// 18. does C have a quickqueck library?
