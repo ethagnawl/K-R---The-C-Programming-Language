690de5a8896f926d45eb04dd3a84e49c7f95eaa9
Peter Doherty <pdoherty926@gmail.com>
2017-01-21 07:13:50 -0500

    chapter-1 - exercise 1-15 - use functions in temperature conversion program

--- chapter-1/temperature-calculator.c
+++ chapter-1/temperature-calculator.c
@@ -4,27 +4,38 @@
 #define UPPER 300
 #define STEP 20
 
-// print Fahrenheit - Celcius table
+// print Fahrenheit - Celsius table
 // for fahr = 0..300
+
+float fahrenheit_to_celsius(float fahrenheit);
+float celsius_to_fahrenheit(float celsius);
 
 main () {
     float fahr, celsius;
 
-    printf("Fahrenheit - Celcius\n");
+    printf("\nFahrenheit - Celsius\n");
     printf("==========\n");
     fahr = UPPER;
     while(fahr >= LOWER) {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        celsius = fahrenheit_to_celsius(fahr);
         printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr - STEP;
     }
 
-    printf("Celcius - Fahrenheit \n");
+    printf("\nCelsius - Fahrenheit\n");
     printf("==========\n");
     celsius = UPPER;
     while(celsius >= LOWER) {
-        fahr = (9.0 / 5.0) * celsius + 32.0;
+        fahr = celsius_to_fahrenheit(celsius);
         printf("%3.0f %6.1f\n", celsius, fahr);
         celsius = celsius - STEP;
     }
+}
+
+float fahrenheit_to_celsius(float fahrenheit) {
+    return (5.0 / 9.0) * (fahrenheit - 32.0);
+}
+
+float celsius_to_fahrenheit(float celsius) {
+    return (9.0 / 5.0) * celsius + 32.0;
 }
